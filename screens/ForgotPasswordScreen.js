88463c4167176f36c1cbd77d3d5f3bb6da0b919c
Ahmonya <ahmonyaedwards@gmail.com>
2019-11-11 12:53:38 -0600

    Frontend Updates
    JavaScript for the Forgot Password and Sign up Screen

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,9 +1,18 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+import { Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    TextInput,
+    Dimensions, Button} from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 
+
+
+const {width: WIDTH} = Dimensions.get('window')
 export default function ForgotPasswordScreen(props) {
     const [email, setEmail] = useState("");
 
@@ -22,26 +31,94 @@ export default function ForgotPasswordScreen(props) {
     }
 
     return (
-        <View style={{paddingTop:50, alignItems:"center"}}>
-
-            <Text>Forgot Password</Text>
-
-            <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+        <View style={styles.backgroundContainer}>
+        
+        <View style={styles.logoContainer}>
+        <Image source={require('../assets/images/icon.png')} style={styles.logo}/>
+        <Text style={styles.logoText}>Forgot Password</Text>
+        </View> 
+        
+        <View style={styles.inputContainer}>
+        <TextInput
+                style={styles.input}
                 value={email}
                 onChangeText={(text) => setEmail(text)}
-                placeholder="Email"
+                placeholder="Email used"
                 keyboardType="email-address"
+                placeholderTextColor={ 'rgba(255,255,255,0.7)'}
                 autoCapitalize="none"
+                underlineColorAndroid='transparent'
                 autoCorrect={false}
             />
 
-            <Button title="Reset Password" onPress={onResetPasswordPress} />
-            <Button title="Back to Login..." onPress={onBackToLoginPress} />
+      <TouchableOpacity style={styles.btnReset} onPress={onResetPasswordPress}>
+           <Text style={styles.text}>Reset Password</Text> 
+        </TouchableOpacity>
+
+        <Button title="Back to Login" onPress={onBackToLoginPress} />
+           
+        </View>
         </View>
     );
     
 }
 
 const styles = StyleSheet.create({
+    backgroundContainer: {
+        flex:1,
+        width: null,
+        height: null, 
+        backgroundColor: '#B5E3FF'    
+          },
+    LogoContainer: {
+        alignItems: 'center',
+            alignContent: 'center',
+            marginBottom: 90
+          },
+          logo:{
+            marginTop: 30,
+            marginLeft: 40,
+            width: 300,
+            height: 300,
+            alignItems:'center',
+            justifyContent: 'center',
+          },
+          logoText:{
+            textAlign: 'center',
+            color:'white',
+            fontSize: 35,
+            fontWeight: '500',
+            opacity: 0.5,
+            alignItems: 'center',
+            fontStyle: 'normal',
+          },
+          inputContainer: {
+            marginTop : 5
+          },
+          input: {
+            width : WIDTH -55,
+            height: 45,
+            borderRadius:25,
+            fontSize: 16,
+            paddingLeft: 20,
+            backgroundColor: '#368DEB',
+            color: 'rgba(255,255,255,0.7)',
+            marginHorizontal: 25
+          },
+          btnReset:{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width : WIDTH -55,
+            height: 45,
+            borderRadius:25,
+            backgroundColor: '#EFA00B',
+            marginTop: 20,
+            marginLeft: 25
+          },
 
+          text:{
+            color: 'rgba(255,255,255,0.7)',
+            fontSize: 16,
+            textAlign: 'center'
+          }
 });
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,9 +1,18 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+import {Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    TextInput,
+    Dimensions,
+    Button  } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 
+
+const {width: WIDTH} = Dimensions.get('window')
 export default function SignupScreen(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -26,49 +35,125 @@ export default function SignupScreen(props) {
 
  
     return (
-        <View style={{paddingTop:50, alignItems:"center"}}>
+        <View style={styles.backgroundContainer}>
+        
+        <View style={styles.logoContainer}>
+        <Image source={require('../assets/images/icon.png')} style={styles.logo}/>
+        <Text style={styles.logoText}>SIGN UP</Text>
+        </View> 
 
-            <Text>Signup</Text>
-
-            <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+        <View style={styles.inputContainer}>
+            
+            <TextInput 
+                style={styles.input}
                 value={email}
                 onChangeText={(text) => setEmail(text)}
-                placeholder="Email"
+                placeholder=".edu Email"
                 keyboardType="email-address"
                 autoCapitalize="none"
+                placeholderTextColor={ 'rgba(255,255,255,0.7)'}
+                underlineColorAndroid='transparent'
                 autoCorrect={false}
             />
 
-            <View style={{paddingTop:10}} />
-
-            <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+            </View>
+            <View style={styles.inputContainer}>
+          <TextInput
+                style={styles.input}
                 value={password}
                 onChangeText={(text) => setPassword(text)}
                 placeholder="Password"
                 secureTextEntry={true}
                 autoCapitalize="none"
                 autoCorrect={false}
+                placeholderTextColor={ 'rgba(255,255,255,0.7)'}
+                underlineColorAndroid='transparent'
             />
+            </View>
 
-            <View style={{paddingTop:10}} />
-
-            <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+            <View style={styles.inputContainer}>
+            <TextInput 
+                style={styles.input}
                 value={passwordConfirm}
                 onChangeText={(text) => setPasswordConfirm(text)}
-                placeholder="Password (confirm)"
+                placeholder="Confirm Password"
                 secureTextEntry={true}
                 autoCapitalize="none"
                 autoCorrect={false}
+                placeholderTextColor={ 'rgba(255,255,255,0.7)'}
+                underlineColorAndroid='transparent'
             />
-
-            <Button title="Signup" onPress={onSignupPress} />
+            </View>
+            <TouchableOpacity style={styles.btnSignUp} onPress={onSignupPress}>
+           <Text style={styles.text}>Signup</Text> 
+        </TouchableOpacity>
 
             <Button title="Back to Login" onPress={onBackToLoginPress} />
         </View>
-        
+
     );
 }
 
 const styles = StyleSheet.create({
-
-});
+    backgroundContainer: {
+        flex:1,
+        width: null,
+        height: null, 
+        backgroundColor: '#B5E3FF'    
+          },
+    LogoContainer: {
+        alignItems: 'center',
+            alignContent: 'center',
+            marginBottom: 90
+          },
+          logo:{
+            marginTop: 30,
+            marginLeft: 40,
+            width: 300,
+            height: 300,
+            alignItems:'center',
+            justifyContent: 'center',
+          },
+          logoText:{
+            textAlign: 'center',
+            color:'white',
+            fontSize: 35,
+            fontWeight: '500',
+            opacity: 0.5,
+            alignItems: 'center',
+            fontStyle: 'normal',
+          },
+          inputContainer: {
+            marginTop : 5
+          },
+          input: {
+            width : WIDTH -55,
+            height: 45,
+            borderRadius:25,
+            fontSize: 16,
+            paddingLeft: 20,
+            backgroundColor: '#368DEB',
+            color: 'rgba(255,255,255,0.7)',
+            marginHorizontal: 25
+          },
+          inputIcon: {
+            position:"relative",
+            top: 8, 
+            left: 37
+          },
+          btnSignUp:{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width : WIDTH -55,
+            height: 45,
+            borderRadius:25,
+            backgroundColor: '#EFA00B',
+            marginTop: 20,
+            marginLeft: 25
+          },
+          text:{
+            color: 'rgba(255,255,255,0.7)',
+            fontSize: 16,
+            textAlign: 'center'
+          }
+    });
